4e733eea280fde8be7a0e8df5997c899685e7c5b
Rokas Reizgys <rokas.student@gmail.com>
2023-09-18 11:02:06 +0300

    Added httpful error surpassing

--- SCMerchantClient/SCMerchantClient.php
+++ SCMerchantClient/SCMerchantClient.php
@@ -65,7 +65,7 @@ class SCMerchantClient
 		if (!$this->debug) {
 			$response = \Httpful\Request::post($this->merchantApiUrl . '/createOrder', $payload, \Httpful\Mime::FORM)->expects(\Httpful\Mime::JSON)->send();
 			if ($response != null) {
-				$body = $response->body;
+				$body = $response->body ?? null;
 				if ($body != null) {
 					if (is_array($body) && count($body) > 0 && isset($body[0]->code)) {
 						return new ApiError($body[0]->code, $body[0]->message);
