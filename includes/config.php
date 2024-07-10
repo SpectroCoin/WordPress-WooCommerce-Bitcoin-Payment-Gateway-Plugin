d5ae5cef3cbf90287eaeb6ad52e54af3fb837c43
Rokas Reizgys <rokas.student@gmail.com>
2024-07-10 10:53:56 +0300

    Update accepted currencies list

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SpectroCoin\Includes;
+
+if (!defined('ABSPATH')) {
+	die('Access denied.');
+}
+
+define("MERCHANT_API_URL", "https://test.spectrocoin.com/api/public");
+define("AUTH_URL", "https://test.spectrocoin.com/api/public/oauth/token");
+define('PUBLIC_SPECTROCOIN_CERT_LOCATION', 'https://test.spectrocoin.com/public.pem');
