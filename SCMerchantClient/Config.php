6ebe95c1582f2a72905aa25af92d8965e7849fa7
Rokas Reizgys <rokas.student@gmail.com>
2024-07-15 13:05:11 +0300

    Config rework

--- SCMerchantClient/Config.php
+++ SCMerchantClient/Config.php
@@ -1,23 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SpectroCoin\SCMerchantClient;
 
 if (!defined('ABSPATH')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
-Class Config{
-	const MERCHANT_API_URL = 'https://test.spectrocoin.com/api/public';
+class Config
+{
+    const MERCHANT_API_URL = 'https://test.spectrocoin.com/api/public';
     const AUTH_URL = 'https://test.spectrocoin.com/api/public/oauth/token';
     const PUBLIC_SPECTROCOIN_CERT_LOCATION = 'https://test.spectrocoin.com/public.pem'; //PROD: https://spectrocoin.com/files/merchant.public.pem
 
-	public static function getPluginFolderName(){
-		$plugin_folder = explode ("/", plugin_basename(__FILE__))[0];
-		return $plugin_folder;
-	}
+    /**
+     * Get the plugin folder name.
+     *
+     * @return string The plugin folder name.
+     */
+    public static function getPluginFolderName(): string
+    {
+        $plugin_folder = explode("/", plugin_basename(__FILE__))[0];
+        return $plugin_folder;
+    }
 
-	// Optional configuration based on CMS:
-	const SPECTROCOIN_REQUIRED_PHP_VERSION = '8.0';
-	const SPECTROCOIN_WP_VERSION = '6.0';
-
+    // Optional configuration based on CMS:
+    const SPECTROCOIN_REQUIRED_PHP_VERSION = '8.0';
+    const SPECTROCOIN_WP_VERSION = '6.0';
 }
