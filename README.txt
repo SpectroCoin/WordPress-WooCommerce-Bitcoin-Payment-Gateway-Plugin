2a20bcf1abfc68a4ebb2c6b84fa397b9f77ca512
Rokas Reizgys <rokas.student@gmail.com>
2024-09-05 11:50:10 +0300

    Update version headers

--- README.txt
+++ README.txt
@@ -6,7 +6,7 @@ Requires at least: 6.2
 Tested up to: 6.6.1
 Stable tag: 1.5.1
 WC requires at least: 7.4
-WC tested up to: 9.1.4
+WC tested up to: 9.2.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- spectrocoin.php
+++ spectrocoin.php
@@ -11,7 +11,7 @@ Requires at least: 6.2
 Requires PHP: 7.4
 Tested up to: 6.6.1
 WC requires at least: 7.4
-WC tested up to: 9.1.4
+WC tested up to: 9.2.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
