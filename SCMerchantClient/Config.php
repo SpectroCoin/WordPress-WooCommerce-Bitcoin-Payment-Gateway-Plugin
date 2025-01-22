073287898c35423038f0b8e7d9c5bbb51064d942
SteinCode <rokas.student@gmail.com>
2025-01-22 12:30:26 +0200

    add public cert

--- SCMerchantClient/Config.php
+++ SCMerchantClient/Config.php
@@ -12,6 +12,7 @@ class Config
 {
     const MERCHANT_API_URL = 'https://spectrocoin.com/api/public';
     const AUTH_URL = 'https://spectrocoin.com/api/public/oauth/token';
+    const PUBLIC_SPECTROCOIN_CERT_LOCATION = 'https://spectrocoin.com/files/merchant.public.pem';
     const ACCEPTED_FIAT_CURRENCIES = ["EUR", "USD", "PLN", "CHF", "SEK", "GBP", "AUD", "CAD", "CZK", "DKK", "NOK"];
 
     // Optional configuration based on CMS:
